9cb79b4cbac6209e0de7cdf733cf0d738f2a7816
icoepspd <itc788031@gmail.com>
2024-09-14 20:50:25 +0530

    Update tablet.py

--- tablet.py
+++ tablet.py
@@ -44,10 +44,14 @@ def ChangeWidgetFontSize(wgt_txt, wch_font_size = '12px'):
 
     components.html(f"{htmlstr}", height=0, width=0)
 
-a = st.columns(3)
-st.write(" ")
-st.write(" ")
-a[1].markdown("<style='text-align: center;'>[Digital Meet Session Summary](https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZWVjYWI4ZGUtMjY3My00OTYxLWFhOTQtZmZmNzY3ZjRkNzdk%40thread.v2/0?context=%7b%22Tid%22%3a%22b5ff47a6-f7b4-4abf-a484-a75057bd8139%22%2c%22Oid%22%3a%22e6de7ade-191f-4b03-96a0-63290e12461e%22%7d)",
+# a = st.columns(3)
+# st.write(" ")
+# st.write(" ")
+#a[1].markdown("<style='text-align: center;'>[Digital Meet Session Summary](https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZWVjYWI4ZGUtMjY3My00OTYxLWFhOTQtZmZmNzY3ZjRkNzdk%40thread.v2/0?context=%7b%22Tid%22%3a%22b5ff47a6-f7b4-4abf-a484-a75057bd8139%22%2c%22Oid%22%3a%22e6de7ade-191f-4b03-96a0-63290e12461e%22%7d)",
+#            unsafe_allow_html=True)
+
+st.markdown("<style='text-align: center;'>[Digital Meet Session Summary](https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZWVjYWI4ZGUtMjY3My00OTYxLWFhOTQtZmZmNzY3ZjRkNzdk%40thread.v2/0?context=%7b%22Tid%22%3a%22b5ff47a6-f7b4-4abf-a484-a75057bd8139%22%2c%22Oid%22%3a%22e6de7ade-191f-4b03-96a0-63290e12461e%22%7d)",
             unsafe_allow_html=True)
+
 
 ChangeWidgetFontSize('Digital Meet Session Summary','30px')
